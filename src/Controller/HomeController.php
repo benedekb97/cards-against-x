297f5228f850147e4c4f714348b691baab096668
Benedek Burgess <benedekb97@gmail.com>
2023-05-31 20:59:13 +0100

    Add create and rejoin buttons to home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,7 +14,16 @@ class HomeController extends AbstractController
     #[Route('/', name: 'index')]
     public function index(): Response
     {
-        return $this->render('index.html.twig');
+        /** @var UserInterface $user */
+        $user = $this->getUser();
+
+        return $this->render(
+            'index.html.twig',
+            [
+                'user' => $user,
+                'game' => $user->getPlayer()?->getGame(),
+            ]
+        );
     }
 
     #[Route('/disclaimer', name: 'disclaimer')]
