7c779817d170901dba2bdb7e8b7f854be8f9370b
benedekburgess <benedek.burgess@cmostores.com>
2023-12-20 09:29:35 +0000

    Add default test

---
+++ tests/RandomTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+class RandomTest extends PHPUnitTestCase
+{
+    public function testExample(): void
+    {
+        $this->assertTrue(true);
+    }
+}
